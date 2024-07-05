fb27ec9efac47eacf80bdb375c1d18fe764a4fde
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2024-07-05 16:26:21 +0900

    feat: update friedman test

--- Matlab/process.m
+++ Matlab/process.m
@@ -25,6 +25,13 @@ d_arr = table2array(ques(:,14:28));
 d_sw = table2array(ques(:,29:43));
 d_hl = table2array(ques(:,44:58));
 
+% use cell to arrange all data group
+d_groups = {d_no, d_arr, d_sw, d_hl};
+for i = 1:length(d_groups)
+    d_groups{i}(:,2) = 8 - d_groups{i}(:,2);
+    d_groups{i}(:,4) = 8 - d_groups{i}(:,4);
+end
+
 % 转置table以匹配boxchart的要求
 % row_ques = rows2vars(ques);
 % row_ques = row_ques(5:46, :);
@@ -71,33 +78,60 @@ fields_sw = fieldnames(s_sw);
 
 % 仅计算每个sub-scale的总平均分和标准差
 
-res = zeros(2, 6);
-res_std = zeros(2, 6);
+res = zeros(4, 6);
+res_std = zeros(4, 6);
 res_diff_p = zeros(1, 6);
-d_mean = zeros(28, 6);
+d_mean = zeros(56, 6);
 x_axis = strings(1, 6);
-last_name = "Efficiency";
+last_name = "SA";
 i = 1;
 j = 1;
-for k=1:numel(fields_2d)
-    name = string(fields_2d(k));
-    names = split(name, '_');
-    if (names(2) == "Natural")
-        names(2) = "Immersion";
-    elseif (k==numel(fields_2d))
-        names(2) = "End";
-        k = k + 1;
+for k=1:(numel(fields_arr)+1)
+    if (k < numel(fields_arr)+1)
+        name = string(fields_arr(k));
+        names = split(name, '_');
+        group_name = names(1);
+        if any(group_name == ["UE1", "UE2", "UE3", "UE4"])
+            group_name = "PQ";
+        elseif any(group_name == ["UE5", "UE6", "UE7", "UE8"])
+            group_name = "HQ";
+        elseif (group_name == "TR2")
+            group_name = "IT";
+        end
+    else
+        group_name = "End";
     end
-    if (names(2) ~= last_name)
+        
+
+    group_name = regexprep(group_name, '[^a-zA-Z]', '');  % clean num in name
+    % calculate mean when the ques group changes
+    if (group_name ~= last_name)  
         x_axis(i) = last_name;
-        last_name = names(2);
-        d_mean(1:14,i) = mean(d_2d(:,j:k-1), 2);
-        d_mean(15:28,i) = mean(d_3d(:,j:k-1), 2);
-        res(1,i) = mean(d_mean(1:14, i));
-        res(2,i) = mean(d_mean(15:28, i));
-        res_std(1,i) = std(d_mean(1:14, i), 1);
-        res_std(2,i) = std(d_mean(15:28, i), 1);
-        res_diff_p(i) = signrank(d_mean(1:14,i), d_mean(15:28,i));
+        last_name = group_name;
+        % mean for each experiment group
+        for m=1:length(d_groups)
+            d_mean(14*m-13:14*m, i) = mean(d_groups{m}(:,j:k-1), 2);
+            res(m,i) = mean(d_mean(14*m-13:14*m, i));
+            res_std(m,i) = std(d_mean(14*m-13:14*m, i), 1);
+            if any(i == [1,2,4])
+                % res_diff_p(i) = signrank(d_mean(1:14,i), d_mean(15:28,i));
+                res_diff_p(i) = friedman([d_mean(1:14,i),d_mean(15:28,i), d_mean(29:42,i), d_mean(43:56,i)], 1, "off");
+            else
+                % res_diff_p(i) = signrank(d_mean(1:14,i), d_mean(15:28,i));
+                res_diff_p(i) = friedman([d_mean(15:28,i), d_mean(29:42,i), d_mean(43:56,i)], 1, "off");
+            end
+        end
+        % d_mean(1:14,i) = mean(d_no(:,j:k-1), 2);
+        % d_mean(15:28,i) = mean(d_arr(:,j:k-1), 2);
+        % d_mean(29:43,i) = mean(d_sw(:,j:k-1), 2);
+        % d_mean(44:58,i) = mean(d_hl(:,j:k-1), 2);
+        % res(1,i) = mean(d_mean(1:14, i));
+        % res(2,i) = mean(d_mean(15:28, i));
+        % res(3,i) = mean(d_mean(29:43, i));
+        % res(4,i) = mean(d_mean(44:58, i));
+        % res_std(1,i) = std(d_mean(1:14, i), 1);
+        % res_std(2,i) = std(d_mean(15:28, i), 1);
+        % res_diff_p(i) = signrank(d_mean(1:14,i), d_mean(15:28,i));
 
         i = i+1;
         j = k;
